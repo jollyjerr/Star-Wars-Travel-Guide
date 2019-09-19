a57050fc4e24df2d414fbaac9b5b906e6ed018c3
jollyjerr <jollyjerr@gmail.com>
2019-09-19 16:02:19 -0600

    remove solution

--- src/components/Planet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Planet(props) {
-    return (
-        <div className="planet">
-            <p>{props.planet.name}</p>
-        </div>
-    )
-}
--- src/components/Starship.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Starship(props) {
-    return (
-        <div className="starship">
-            <p> {props.starship.name} </p>
-        </div>
-    )
-}
